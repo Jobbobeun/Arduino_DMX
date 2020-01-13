b9292dd47166b012f580b2443f92fb29dc40b2a7
Job Heijlighen <info@jobbomodels.nl>
2020-01-13 18:56:54 +0100

    Remove comments

--- Arduino_DMX.ino
+++ Arduino_DMX.ino
@@ -1,9 +1,11 @@
-//************************************************************************************************************************
-#include <lib_dmx.h>  // libreria DMX 4 universos   // deskontrol four universes DMX library  - www.deskontrol.net/blog
+/* Project: DZ smoke machines 2020
+   Author:  Job Heijlighen
+   Date:    13-01-20
+   Version: 1.0
+*/
 
-//*********************************************************************************************************
-//                        New DMX modes *** EXPERIMENTAL ***
-//*********************************************************************************************************
+#include <lib_dmx.h> 
+
 #define    DMX512     (0)    // (250 kbaud - 2 to 512 channels) Standard USITT DMX-512
 #define    DMX1024    (1)    // (500 kbaud - 2 to 1024 channels) Completely non standard - TESTED ok
 #define    DMX2048    (2)    // (1000 kbaud - 2 to 2048 channels) called by manufacturers DMX1000K, DMX 4x or DMX 1M ???
@@ -15,23 +17,14 @@ bool set;
 
 void setup()
 {
-  // configurar pines arduino del 2 al 13 como entradas con pullup, (cuando se pulsa el boton = 0 si no = 1)
-  // configure arduino pins 2 to 13 as inputs with pullup, (button pressed = 0, button free = 1)
-  pinMode(trigger, INPUT);           // pines como entradas
-  // pins as inputs
-  digitalWrite(trigger, HIGH);       // activar resistencias pullup internas
-  // turn on pullup internal resistors
-trigger_old =true;
 
-  ArduinoDmx0.set_tx_address(1);      // poner aqui la direccion de inicio de DMX
-  // put here DMX start address
-
-  ArduinoDmx0.set_tx_channels(100);   // poner aqui el numero de canales a transmitir
-  // put here the number of DMX channels to transmmit
-
-  ArduinoDmx0.init_tx(DMX512);        // iniciar transmision universo 0, modo estandar DMX512
-  // starts universe 0 as TX, standard mode DMX512
-}  //end setup()
+  pinMode(trigger, INPUT);
+  digitalWrite(trigger, HIGH);
+  trigger_old = true;
+  ArduinoDmx0.set_tx_address(1);
+  ArduinoDmx0.set_tx_channels(100);
+  ArduinoDmx0.init_tx(DMX512);
+}
 
 void loop()
 {
@@ -62,7 +55,4 @@ void loop()
   }
 
 
-}  //end loop()
-
-
-//************************************************************************************************************************
+}  
