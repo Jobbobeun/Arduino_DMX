97bd37ed27863864b346bdaf0aa29e5970b22b9e
Job Heijlighen <info@jobbomodels.nl>
2020-01-13 18:53:21 +0100

    Turn dmx channel 1-2-3-4 to 255 when input 4 is LOW (GND).

--- Arduino_DMX.ino
+++ Arduino_DMX.ino
@@ -7,7 +7,11 @@
 #define    DMX512     (0)    // (250 kbaud - 2 to 512 channels) Standard USITT DMX-512
 #define    DMX1024    (1)    // (500 kbaud - 2 to 1024 channels) Completely non standard - TESTED ok
 #define    DMX2048    (2)    // (1000 kbaud - 2 to 2048 channels) called by manufacturers DMX1000K, DMX 4x or DMX 1M ???
-#define trigger 4
+#define     trigger 4
+
+bool trigger_state;
+bool trigger_old;
+bool set;
 
 void setup()
 {
@@ -17,7 +21,7 @@ void setup()
   // pins as inputs
   digitalWrite(trigger, HIGH);       // activar resistencias pullup internas
   // turn on pullup internal resistors
-
+trigger_old =true;
 
   ArduinoDmx0.set_tx_address(1);      // poner aqui la direccion de inicio de DMX
   // put here DMX start address
@@ -31,15 +35,32 @@ void setup()
 
 void loop()
 {
-  if (digitalRead(trigger) == LOW) {
 
+  if (digitalRead(trigger) == LOW) {
+    trigger_state = false;
+  } else {
+    trigger_state = true;
+  }
+
+  if (trigger_state != trigger_old) {
+    set = true;
+    trigger_old = trigger_state;
+  }
+
+  if (set && (trigger_state == false)) {
     ArduinoDmx0.TxBuffer[0] = 255;
     ArduinoDmx0.TxBuffer[1] = 255;
     ArduinoDmx0.TxBuffer[2] = 255;
-    delay(20000);
+    ArduinoDmx0.TxBuffer[3] = 255;
+    set = false;
+  } else if (set && (trigger_state == true)) {
     ArduinoDmx0.TxBuffer[0] = 0;
     ArduinoDmx0.TxBuffer[1] = 0;
-    }
+    ArduinoDmx0.TxBuffer[2] = 0;
+    ArduinoDmx0.TxBuffer[3] = 0;
+    set = false;
+  }
+
 
 }  //end loop()
 
